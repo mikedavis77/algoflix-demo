6a7ff690bdab8a52864c2cd9c226a6060b6d83a2
Mike Davis <mikedavisis@gmail.com>
2022-02-01 10:24:19 +0000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,208 @@
+const { algoliasearch, instantsearch } = window;
+
+const searchClient = algoliasearch(
+  'latency',
+  'af044fb0788d6bb15f807e4420592bc5'
+);
+
+const search = instantsearch({
+  indexName: 'algoflix',
+  searchClient,
+  //routing: true,
+  routing: {
+    stateMapping: instantsearch.stateMappings.singleIndex('algoflix'),
+  },
+  /* routing: {
+    router: instantsearch.routers.history({
+      windowTitle({ query }) {
+        return query ? `Results for "${query}"` : 'Search';
+      },
+
+      createURL({ qsModule, routeState, location }) {
+        const urlParts = location.href.match(/^(.*?)/);
+        const baseUrl = `${urlParts ? urlParts[1] : ''}/`;
+        //console.log('cU:', routeState, urlParts, baseUrl);
+
+        const queryParameters = {};
+        if (routeState.query) {
+          queryParameters.query = encodeURIComponent(routeState.query);
+        }
+        if (routeState.page !== 1) {
+          queryParameters.page = routeState.page;
+        }
+        if (routeState.year) {
+          queryParameters.year = routeState.year.map(encodeURIComponent);
+        }
+        if (routeState.genres) {
+          queryParameters.genres = routeState.genres.map(encodeURIComponent);
+        }
+        if (routeState.price) {
+          queryParameters.price = routeState.price.map(encodeURIComponent);
+        }
+        if (routeState.priceRange) {
+          queryParameters.priceRange = routeState.priceRange;
+        }
+
+        const queryString = qsModule.stringify(queryParameters, {
+          addQueryPrefix: true,
+          arrayFormat: 'repeat',
+        });
+
+        return `${baseUrl}${queryString}`;
+      },
+
+      parseURL({ qsModule, location }) {
+        //console.log('pU:', location);
+        const {
+          query = '',
+          page,
+          year = [],
+          genres = [],
+          price = [],
+          priceRange = '',
+        } = qsModule.parse(location.search.slice(1));
+        //console.log('search', location.search.slice(1));
+        // `qs` does not return an array when there's a single value.
+        const allYears = Array.isArray(year) ? year : [year].filter(Boolean);
+        const allGenres = Array.isArray(genres)
+          ? genres
+          : [genres].filter(Boolean);
+        const allPrices = Array.isArray(price)
+          ? price
+          : [price].filter(Boolean);
+        //console.log('data', allYears, allGenres, allPrices, priceRange);
+
+        return {
+          query: decodeURIComponent(query),
+          page,
+          year: allYears.map(decodeURIComponent),
+          genres: allGenres.map(decodeURIComponent),
+          price: allPrices.map(decodeURIComponent),
+          priceRange,
+        };
+      },
+    }),
+
+    stateMapping: {
+      stateToRoute(uiState) {
+        const indexUiState = uiState['algoflix'] || {};
+        //console.log('sr:', uiState);
+
+        return {
+          query: indexUiState.query,
+          page: indexUiState.page,
+          year: indexUiState.refinementList && indexUiState.refinementList.year,
+          genres:
+            indexUiState.refinementList &&
+            indexUiState.refinementList['genres.en-US'],
+          price:
+            indexUiState.refinementList && indexUiState.refinementList.price,
+          priceRange: indexUiState.range && indexUiState.range.price,
+        };
+      },
+
+      routeToState(routeState) {
+        //console.log('routeToState:', routeState);
+        return {
+          algoflix: {
+            query: routeState.query,
+            page: routeState.page,
+            refinementList: {
+              year: routeState.year,
+              'genres.en-US': routeState.genres,
+              price: routeState.price,
+            },
+            range: { price: routeState.priceRange },
+          },
+        };
+      },
+    },
+    //stateMapping: instantsearch.stateMappings.singleIndex('algoflix'),
+  }, */
+});
+
+search.addWidgets([
+  instantsearch.widgets.configure({
+    hitsPerPage: 9,
+  }),
+  instantsearch.widgets.clearRefinements({
+    container: '#clear-refinements',
+  }),
+  instantsearch.widgets.currentRefinements({
+    container: '#current-refinements',
+  }),
+  instantsearch.widgets.refinementList({
+    container: '#year',
+    attribute: 'year',
+    limit: 5,
+    showMore: true,
+  }),
+  instantsearch.widgets.refinementList({
+    container: '#price',
+    attribute: 'price',
+  }),
+  instantsearch.widgets.rangeSlider({
+    container: '#price-slider',
+    attribute: 'price',
+    pips: false,
+    tooltips: {
+      format(value) {
+        if (value < 3) return 'Low';
+        if (value < 7) return 'Mid';
+        if (value >= 7) return 'High';
+        return value;
+      },
+    },
+  }),
+  instantsearch.widgets.refinementList({
+    container: '#genres',
+    attribute: 'genres.en-US',
+  }),
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      /* item: hit => `
+        <div>
+          <img src="${hit.backdrop}" align="left" alt="${hit.title}" />
+          <div class="hit-name">
+            ${instantsearch.highlight({ attribute: 'title.en-US', hit })}, ${
+        hit.year
+      }
+          </div>
+          <div class="hit-price">\$${hit.price}</div>
+        </div>
+      `, */
+      item: (hit, bindEvent) => `
+        <div>
+          <img src="${hit.backdrop}" align="left" alt="${hit.title}" />
+          <div class="hit-name">
+            ${instantsearch.highlight({ attribute: 'title.en-US', hit })}, ${
+        hit.year
+      }
+          </div>
+          <div class="hit-price">\$${hit.price}</div>
+          <div class="hit-add-cart"><button ${bindEvent(
+            'click',
+            hit,
+            'add-to-cart'
+          )}>Add to Cart</button></div>
+        </div>
+      `,
+    },
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  }),
+]);
+
+search.use(
+  instantsearch.middlewares.createInsightsMiddleware({
+    insightsClient: window.aa,
+  })
+);
+//window.aa('setUserToken', 'my-user-token');
+
+search.start();
